2c5015af98c92fe61110404f55e75cedb02d86d9
田鑫宇 <tianxy0711@163.1com>
2024-06-19 13:59:59 +0800

    springboot多环境设置不同的配置端口

---
+++ springboot-007-multi-enveriment/src/main/java/com/powernode/controller/BootController.java
@@ -0,0 +1,23 @@
+package com.powernode.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * packageName com.powernode.controller
+ *
+ * @author 田鑫宇
+ * @version JDK 17
+ * @className BootController
+ * @date 2024/6/19
+ * @description TODO
+ */
+@Controller
+public class BootController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String doSome(){
+        return "springboot多环境配置";
+    }
+}
